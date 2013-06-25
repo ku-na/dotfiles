76bdfbd5def4f03aaa377b02ad91ecaa85dbda05
nao <kuroiwanao@rtri.or.jp>
2013-06-25 14:36:09 +0900

    make this up-to-date

--- bin/backup.sh
+++ bin/backup.sh
@@ -1,23 +1,27 @@
 #!/bin/sh
-# log of backup
-LOG=/home/kuroiwa/epson-156/Data/backup_log
-echo "===== BACKUP STARTED `date +%Y/%m/%d/%H:%M:%S` =====" >> $LOG
-# where to backup
-PARTITION=/dev/sdb2
-HOME_DIR=/home
-HOME_USR=`ls $HOME_DIR`
-echo $HOME_DIR
-echo $HOME_USR
-HOME_USR=kuroiwa
 
-for user in $HOME_USR; do
-	tar cvjf /tmp/backup_$user_`date +%y%m%dH%M`.tar.bz2 $HOME_DIR/$user/ >> $LOG
-	cd /tmp
-	echo "--- Backup File ---" >> $LOG
-	ls backup_* >> $LOG
-	mv backup_$user_* /home/kuroiwa/share/epson-156/Data
-	/bin/rm -f *.tar.bz2 >> $LOG
-	echo "-------------------" >> $LOG
-	echo `date` >> $LOG
-	echo "-------------------" >> $LOG
+users=`ls /home/`
+today=`date +%Y%m%d`
+ex_file="/home/kuroiwa/.backup_ex"
+backup_home="/home/kuroiwa/share/cmst01/backup"
+bogus="$backup_home/bogus"
+
+cmd_compress="rar"
+opt_compress="a -r -ol -x@${ex_file}"
+
+echo "mkdir $bogus"
+mkdir $bogus
+echo "mv ${backup_home}/*.{rar,log} ${bogus}/"
+mv ${backup_home}/*.{rar,log} ${bogus}/
+#
+cd /home
+for user in $users; do
+	echo "compressing [${user}]'s home..."
+	$cmd_compress $opt_compress ${backup_home}/${today}_${user}.rar $user/ > ${backup_home}/${today}_${user}.log
 done
+#
+echo "compressing [epson-156]..."
+$cmd_compress $opt_compress ${backup_home}/${today}_epson-156.rar kuroiwa/share/epson-156/ > ${backup_home}/${today}_epson-156.log
+#
+echo "/bin/rm -rf $bogus"
+/bin/rm -rf $bogus
